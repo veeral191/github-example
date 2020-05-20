26a77ca88603c6cce850bac820fb2ee15dabd94c
Shah <viral.c.shah@capgemini.com>
2020-05-19 19:27:32 -0500

    assignment for capstone project

---
+++ Notebook.py
@@ -0,0 +1,6 @@
+# --------------------- This is only used for Capstone projects -------- #
+
+import numpy as np  # useful for many scientific computing in Python
+import pandas as pd  # primary data structure library
+
+print('Hello Capstone Project Course!')
